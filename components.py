4197bbc0b355f18b13e87b71f9b2e29f4587cbb0
Daniel Stoyell <Daniel Stoyell>
2017-04-30 14:30:48 -0400

    restructure

--- components.py
+++ components.py
@@ -1,4 +1,4 @@
-''' Stores the classes for Player, Board, and Bag
+''' Stores the classes for Player, Board, Bag, and Game
 ### TODO ###
 	1. Implement Bag
 	2. Implement Board
@@ -7,143 +7,156 @@
 
 import random
 
-class gameData
-	letter_values = {
-		"a": 1,
-		"b": 1,
-		"c": 1,
-		"d": 1,
-		"e": 1,
-		"f": 1,
-		"g": 1,
-		"h": 1,
-		"i": 1,
-		"j": 1,
-		"k": 1,
-		"l": 1,
-		"m": 1,
-		"n": 1,
-		"o": 1,
-		"p": 1,
-		"q": 1,
-		"r": 1,
-		"s": 1,
-		"t": 1,
-		"u": 1,
-		"v": 1,
-		"w": 1,
-		"x": 1,
-		"y": 1,
-		"z": 1
-	}
+#### GENERAL GAME DATA ####
 
-	def get_letter_value(self, letter):
-		return letter_values[letter]
+dictFile = open("dictionary.txt", "r")
+dictionary = set()
+for line in dictFile:
+	dictionary.add(line.strip())
+dictFile.close()
+
+letter_values = {
+	"a": 1,
+	"b": 3,
+	"c": 3,
+	"d": 2,
+	"e": 1,
+	"f": 4,
+	"g": 2,
+	"h": 4,
+	"i": 1,
+	"j": 8,
+	"k": 5,
+	"l": 1,
+	"m": 3,
+	"n": 1,
+	"o": 1,
+	"p": 3,
+	"q": 10,
+	"r": 1,
+	"s": 1,
+	"t": 1,
+	"u": 1,
+	"v": 4,
+	"w": 4,
+	"x": 8,
+	"y": 4,
+	"z": 10,
+	"?": 0
+}
+
+modifiers = [['TW','  ','  ','DL','  ','  ','  ','TW','  ','  ','  ','DL','  ','  ','TW'],
+			 ['  ','DW','  ','  ','  ','TL','  ','  ','  ','TL','  ','  ','  ','DW','  '],
+			 ['  ','  ','DW','  ','  ','  ','DL','  ','DL','  ','  ','  ','DW','  ','  '],
+			 ['DL','  ','  ','DW','  ','  ','  ','DL','  ','  ','  ','DW','  ','  ','DL'],
+			 ['  ','  ','  ','  ','DW','  ','  ','  ','  ','  ','DW','  ','  ','  ','  '],
+			 ['  ','TL','  ','  ','  ','TL','  ','  ','  ','TL','  ','  ','  ','TL','  '],
+			 ['  ','  ','DL','  ','  ','  ','DL','  ','DL','  ','  ','  ','DL','  ','  '],
+			 ['TW','  ','  ','DL','  ','  ','  ','DW','  ','  ','  ','DL','  ','  ','TW'],
+			 ['  ','  ','DL','  ','  ','  ','DL','  ','DL','  ','  ','  ','DL','  ','  '],
+			 ['  ','TL','  ','  ','  ','TL','  ','  ','  ','TL','  ','  ','  ','TL','  '],
+			 ['  ','  ','  ','  ','DW','  ','  ','  ','  ','  ','DW','  ','  ','  ','  '],
+			 ['DL','  ','  ','DW','  ','  ','  ','DL','  ','  ','  ','DW','  ','  ','DL'],
+			 ['  ','  ','DW','  ','  ','  ','DL','  ','DL','  ','  ','  ','DW','  ','  '],
+			 ['  ','DW','  ','  ','  ','TL','  ','  ','  ','TL','  ','  ','  ','DW','  '],
+			 ['TW','  ','  ','DL','  ','  ','  ','TW','  ','  ','  ','DL','  ','  ','TW'],]
+
+modifier_colors = {
+	"TW": "#F26243",
+	"DW": "#FDBAA9",
+	"TL": "#3FA0BB",
+	"DL": "#BDDAD5",
+	"  ": "#CEC5A8",
+}
+
+def get_letter_value(self, letter):
+	return letter_values[letter]
 
 class Player:
-	def __init__(self, name, start_tiles, is_ai):
+	def __init__(self, name, start_tiles, ai):
 		self.score = 0
 		self.name = name
 		self.rack = start_tiles
-		self.is_ai = is_ai
+		self.ai = ai
 
 	def get_score(self):
 		return self.score
 
 	def is_ai(self):
-		return self.is_ai
+		return self.ai
 
 	def get_name(self):
 		return self.name
 
+	def get_tiles(self):
+		return self.rack
+
+	def get_tile_rep(self):
+		output = ""
+		delim = ""
+		for tile in self.rack:
+			output += delim + tile
+			delim = ", "
+		return output
+
 	def find_move(self, board):
-		if self.is_ai:
+		if self.ai:
 			# Fancy machine learning and artificial intelligence goes here
-			pass
+			print("I am a robot and I am finding a move")
+			return (False, False, False)
 		else:
 			# Stupid old human decision-making goes here
-			pass
+			print("Grab human move data and return human move....")
+			return (False, False, False)
 
 class Board:
 	def __init__(self):
-		self.board = [['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'],
-					  ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_']]
-		self.modifiers = [['TW','__','__','__','__','__','__','TW','__','__','__','__','__','__','TW'],
-						  ['__','DW','__','__','__','__','__','__','__','__','__','__','__','DW','__'],
-						  ['__','__','DW','__','__','__','__','__','__','__','__','__','DW','__','__'],
-						  ['__','__','__','DW','__','__','__','__','__','__','__','DW','__','__','__'],
-						  ['__','__','__','__','DW','__','__','__','__','__','DW','__','__','__','__'],
-						  ['__','__','__','__','__','__','__','__','__','__','__','__','__','__','__'],
-						  ['__','__','__','__','__','__','__','__','__','__','__','__','__','__','__'],
-						  ['TW','__','__','__','__','__','__','DW','__','__','__','__','__','__','TW'],
-						  ['__','__','__','__','__','__','__','__','__','__','__','__','__','__','__'],
-						  ['__','__','__','__','__','__','__','__','__','__','__','__','__','__','__'],
-						  ['__','__','__','__','DW','__','__','__','__','__','DW','__','__','__','__'],
-						  ['__','__','__','DW','__','__','__','__','__','__','__','DW','__','__','__'],
-						  ['__','__','DW','__','__','__','__','__','__','__','__','__','DW','__','__'],
-						  ['__','DW','__','__','__','__','__','__','__','__','__','__','__','DW','__'],
-						  ['TW','__','__','__','__','__','__','TW','__','__','__','__','__','__','TW'],]
+		self.board = [[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+					  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']]
 
 	def set_square(self, square, letter):
-		if self.board[square[0]][square[1]] == '_':
-			self.board[square[0]][square[1]] = letter
-			return True
-		else:
-			return False
+		self.board[square[0]][square[1]] = letter
 
 	def get_square(self, square):
 		return self.board[square[0]][square[1]]
 
-	def get_square_modifier(self, square):
-		return self.modifiers[square[0]][square[1]]
-
-	def play_move(self, word, start_square, direction, dictionary):
-		is_valid = self.is_valid_move(word, start_square, direction, dictionary)
-		if not is_valid[0]:
-			return is_valid
-		score = 0
-		multiplier = 1
+	def play_move(self, word, start_square, direction):
 		current_square = start_square
-		for letter in word.split():
-			if self.get_square(current_square) = "_":
-				mod = self.get_square_modifier(current_square)
-				if mod[1] == "L":
-					score += gameData.get_letter_value(letter) * int(mod[0])
-				elif mod[1] == "W":
-					multiplier *= int(mod[0])
-				else:
-					score += gameData.get_letter_value(letter)
-			elif self.get_square(current_square) == letter:
-				score += gameData.get_letter_value(letter)
+		letters_used = []
+		for i in range(len(word)):
+			if self.get_square(current_square) == " ":
+				self.set_square(current_square, word[i])
+				letters_used.append(word[i])
+			if direction == "Vertical":
+				current_square[0] += 1
 			else:
-				return (False, "You fucked up")
-		return (True, score*multiplier)
+				current_square[1] += 1
+		return 10, letters_used
 
-	def is_valid_move(self, word, start_square, dictionary):
-		if not self.is_valid_square(current_square):
-			return (False, "Invalid square (outside boundaries of board)")
-		elif self.get_square(current_square) != letter:
-			return (False, "Tiles already placed that are not compatible with word")
+	def is_valid_move(self, word, direction, square):
+		return True, "Invalid move! Try again!"
+		# if not self.is_valid_square(current_square):
+		# 	return (False, "Invalid square (outside boundaries of board)")
+		# elif self.get_square(current_square) != letter:
+		# 	return (False, "Tiles already placed that are not compatible with word")
 
-
-
-		return (True, score)
+		# return (True, score)
 
 	def is_valid_square(self, square):
-		return square[0] >= 0 and square[1] >=0 and square[0] <= 15 and square[1] <= 15
+		return square[0] >= 0 and square[1] >=0 and square[0] <= 14 and square[1] <= 14
 
 class Bag:
 	def __init__(self):
@@ -155,7 +168,7 @@ class Bag:
 			+  ['O'] * 8 \
 			+  ['A','I'] * 9 \
 			+  ['E'] * 12
-		random.shuffle(bag)
+		random.shuffle(self.bag)
 
 	def draw_tiles(self, n):
 		drawn = self.bag[-n:]
